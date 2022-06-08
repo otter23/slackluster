cc2dded4da19b67a54b43947fd3b83de43da780e
Elan Katz <otter2388@gmail.com>
2022-06-08 11:40:37 -0700

    add socket-io to front end via a chat component

---
+++ frontend/src/socket.js
@@ -0,0 +1,8 @@
+//import client-side socket
+// import { io } from 'socket.io-client';
+
+// initialize a new instance of socket.io by passing the server (the HTTP server) object.
+//defaults to trying to connect to the host that serves the page.
+// const socket = io();
+
+// export default socket;
