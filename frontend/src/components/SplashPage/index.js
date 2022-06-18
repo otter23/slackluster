774449b5217376295f181fd9375b42faf04caf02
Elan Katz <otter2388@gmail.com>
2022-06-17 23:32:04 -0700

    fix splash and footer for mobile

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -14,23 +14,24 @@ export default function SplashPage({ isLoaded }) {
   return (
     <>
       <NavBarSplash />
-      <div
-        className='splash-background'
-        // style={{ backgroundImage: `url(${image})` }}
-      >
-        <div className='splash-body-container'>
-          <div className='splash-header-image'></div>
-          <h1 className='splash-header'>Slackluster is your digital HQ</h1>
-          <h2 className='splash-header-two'>
-            Transform the way you work with one place for everyone and
-            everything you need to get stuff done.
-          </h2>
-          <Link to='/get-started' className='splash-try-for-free'>
-            TRY FOR FREE
-          </Link>
-        </div>
+      <div className='splash-main-container'>
+        <div
+          className='splash-background'
+          // style={{ backgroundImage: `url(${image})` }}
+        >
+          <div className='splash-body-container'>
+            <div className='splash-header-image'></div>
+            <h1 className='splash-header'>Slackluster is your digital HQ</h1>
+            <h2 className='splash-header-two'>
+              Transform the way you work with one place for everyone and
+              everything you need to get stuff done.
+            </h2>
+            <Link to='/get-started' className='splash-try-for-free'>
+              TRY FOR FREE
+            </Link>
+          </div>
 
-        {/* <div className='splash-photo-meta-container'>
+          {/* <div className='splash-photo-meta-container'>
           <div className='splash-photo-title'>
             <a href='https://www.flickr.com/photos/137364640@N08/51103074015/'>
               Slackluster
@@ -42,9 +43,10 @@ export default function SplashPage({ isLoaded }) {
             </a>
           </div>
         </div> */}
-      </div>
+        </div>
 
-      <Footer />
+        <Footer />
+      </div>
     </>
   );
 }
